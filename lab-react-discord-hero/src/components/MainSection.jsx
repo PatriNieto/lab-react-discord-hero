03da9ad9f52846913ab8a4ab88b3147cf09a3829
PatriNieto <patricianieto2.0@gmail.com>
2024-09-16 19:02:02 +0200

    Done right backgroundImage

--- lab-react-discord-hero/src/components/MainSection.jsx
+++ lab-react-discord-hero/src/components/MainSection.jsx
@@ -1,19 +1,22 @@
 
-import FooterImg from './FooterImg.jsx'
 import MainSectionInfo from './MainSectionInfo.jsx'
-
+import backImg from "../assets/discord-background.png"
 
 const styleMainDiv = {
   display: "flex",
   alignItems: "center",
-  backgroundImage: "url('./assets/discord-background.png')" 
+  height:"100vh",
+  width:"100vw",
+  backgroundImage: `url("${backImg}")`,
+  backgroundPosition:"bottom right",
+  backgroundRepeat: "no-repeat"
 }
 
 function MainSection(){
   return (
     <div style={styleMainDiv}>
       <MainSectionInfo />
-      <FooterImg />
+      
     </div>
   )
 }
